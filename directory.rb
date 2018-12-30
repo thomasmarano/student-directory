6db78a81d60ab1d56f967c75007d9f2dc7bbbbb1
Thomas <thomas.marano2@gmail.com>
2018-12-30 15:34:59 +0100

    First Set of Exercises

--- directory.rb
+++ directory.rb
@@ -5,8 +5,16 @@ def input_students
     name = gets.chomp
 
     while !name.empty? do
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
+        puts "What is their country of birth?"
+        birth = gets.chomp
+        puts "What is your cohort?"
+        cohort = gets.chomp
+        students << {name: name, cohort: cohort, birth: birth}
+        if students.length == 1
+            puts "Now we have 1 student"
+        else
+            puts "Now we have #{students.count} students"
+        end
         name = gets.chomp
     end
     students
@@ -18,12 +26,16 @@ def print_header
 end
 
 def print(names)
-    names.each do |name|
-        puts "#{name[:name]} (#{name[:cohort]} cohort)"
+    counter = 0
+    while counter < names.length do
+          if (names[counter][:name][0] == 'T' && names[counter][:name].length < 12)
+              puts "#{names[counter][:name]} (#{names[counter][:cohort]} cohort) and #{names[counter][:birth]}".center(100)
+          end
+          counter += 1
     end
 end
 def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
+    puts "Overall, we have #{names.count} great students".center(100)
 end
 students = input_students
 print_header
